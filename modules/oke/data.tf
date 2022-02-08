f45b0a7bf0753168f40c36fdd7b0f54a6e8f03dd
Zachary Smith <Zachary.Smith@oracle.com>
2022-02-08 15:39:34 -0600

    Fix for Issue #1

--- modules/oke/data.tf
+++ modules/oke/data.tf
@@ -4,5 +4,5 @@ data "oci_identity_availability_domains" "ads" {
 }
 
 locals {
-  flex_shape = var.is_flex_shape ? [{ memory_in_gbs = var.node_pool_node_shape_config_memory_in_gbs, ocpus = var.node_pool_node_shape_config_ocpus }] : []
+  flex_shape = var.is_flex_node_shape ? [{ memory_in_gbs = var.node_pool_node_shape_config_memory_in_gbs, ocpus = var.node_pool_node_shape_config_ocpus }] : []
 }
